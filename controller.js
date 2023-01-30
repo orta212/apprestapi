04beba0d5dab2e863783b0506a19c5c2d062de70
orta212 <ortayamaesa9122@gmail.com>
2023-01-30 16:59:58 +0700

    tampil data mahasiswa

--- controller.js
+++ controller.js
@@ -4,5 +4,5 @@ let response = require('./res')
 let connection = require('./koneksi')
 
 exports.index = ((req, res) => {
-    response.ok("Aplikasi REST API berjalan!")
+    response.ok("Aplikasi REST API berjalan!", res)
 })
--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app.use(bodyParser.json())
 let routes = require('./routes')
 routes(app)
 
-
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 })
