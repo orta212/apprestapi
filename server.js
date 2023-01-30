356fbbc4d85678d71aa9b39a87a5560540719979
orta212 <ortayamaesa9122@gmail.com>
2023-01-30 16:06:54 +0700

    menambahkan file koneksi

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+const port = 8000
+//parse application/json
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+})
+
